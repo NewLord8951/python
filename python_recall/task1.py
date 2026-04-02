c380308a0c9d341d0258ee0ee0b6fd594452d83c
New_Lord_8951 <alexandrmaniraki@gmail.com>
2026-04-02 09:47:51 +0300

    Не работает

--- python_recall/task1.py
+++ python_recall/task1.py
@@ -1,12 +1,10 @@
 from random import randint
 
 class Library():
-    def __init__(self, title="", author="", year=0, avaiable=True, times_borrowed=0):
+    def __init__(self, title="", author="", year=0):
         self.title = title
         self.author = author
         self.year = year
-        self.avaiable = avaiable
-        self.times_borrowed = times_borrowed
     
     def add_book(self):
         try:
@@ -14,7 +12,7 @@ class Library():
             isbn = randint(1000000000000, 9999999999999)
             self.isbn_string = str(isbn)
             self.title = input("Введите название книги: ")
-            self.author = input("введите автора книги: ")
+            self.author = input("Введите автора книги: ")
             self.year = int(input("Введите дату написания книги: "))
             if(self.year < 1900 or self.year > 2026):
                 self.year = int(input("Введите дату от 1900 до 2026: "))
@@ -27,17 +25,32 @@ class Library():
             return book
         except:
             print("Cheese")
+            
+    def search_book():
+        # try:
+            search_book = int(input("Ввдеите: 1(Поиск по названию), 2(Поиск по автору), 3(Поиск по году написания) "))
+            if(search_book == 1):
+                return 0
+            elif(search_book == 2):
+                return 1
+            elif(search_book == 3):
+                return 2
+        # except:
+        #     print("A")
 
 list_list = []
 while True:
-    try:
-        vvod = int(input("Введите: 1(Ввести книгу), 2(Вывести книгу) 0(Выйти) "))
+    # try:
+        vvod = int(input("Введите: 1(Ввести книгу), 2(Вывести книгу), 3(Найти книгу) 0(Выйти) "))
+        book_ = Library()
+        ll0 = list_list[]
         if(vvod == 1):
-            book_ = Library()
             list_list.append(book_.add_book())
         elif(vvod == 2):
             print(list_list)
+        elif(vvod == 3):
+            print(ll0)
         elif(vvod == 0):
             break
-    except:
-        print("Hello World")
+    # except:
+    #     print("Hello World")
